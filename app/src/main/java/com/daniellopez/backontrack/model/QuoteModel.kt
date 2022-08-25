635d292e85df8ab74cbb164de8af45501c546cb8
Daniel M. López <dlopeztorresmalaga@gmail.com>
2022-08-25 14:34:59 +0200

    First Commit

---
+++ app/src/main/java/com/daniellopez/backontrack/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.daniellopez.backontrack.model
+
+data class QuoteModel(val quote:String, val author:String)
---
+++ app/src/main/java/com/daniellopez/backontrack/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.daniellopez.backontrack.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.daniellopez.backontrack.databinding.ActivityMainBinding
+import com.daniellopez.backontrack.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    private val quoteViewModel : QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer {
+            binding.tvQuote.text = it.quote
+            binding.tvAuthor.text = it.author
+        })
+
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+    }
+}
